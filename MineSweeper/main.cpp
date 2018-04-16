f94b60bff30b9b32b3db872d4b9589bfb9f4c979
Charile Weissman <cweissma@udel.edu>
2018-04-15 21:07:15 -0400

    Initial Commit

---
+++ MineSweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MineSweeper
+//
+//  Created by cweissma on 4/15/18.
+//  Copyright © 2018 cweissma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
