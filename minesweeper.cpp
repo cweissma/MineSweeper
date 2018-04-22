7e2a0fb1ec5b78dca4eae6d4c4a32560f9be9151
Charile Weissman <cweissma@udel.edu>
2018-04-21 23:15:52 -0400

    Places bombs and successfully finds them and quits if you lose

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -14,52 +14,27 @@ using namespace std;
 //********* Declaration **************
 //************************************
 
-//--Functions
+//--Declare Functions
 int GetBoardSize(int x, int y);
 int SetupInitialBoards(int BoardDim);
-int OutputBoard();
+int OutputPublicBoard();
+int OutputPrivateBoard();
 int PlantMines();
 int MakeAMove();
 int ProcessMove();
+int PressEnter();
+void OutputStatus(bool here);
 
-//--Variables
+//--Declare Variables
 char PrivateBoard[8][8];
 char PublicBoard[8][8];
 char MineField[64];
 int BoardDim=8;
 int BoardSize = BoardDim * BoardDim;
-int MineSize=21;
+int NumOfMines=21;
+bool StillPlaying = true;
 int xMove=0;
 int yMove=0;
-
-
-
-
-//************************************
-//************ Main ******************
-//************************************
-
-int main() {
-    cout << "BoardDim: " << BoardDim << " x " << BoardDim << endl << "# of Mines: " << MineSize << endl;
-    SetupInitialBoards(BoardDim);
-    
-    OutputBoard();
-    PlantMines();
-    enum Status {Active, Won, Lost};
-    Status Alive = Active;
-    switch(Alive) {
-        case Active  : cout << "Your Status is - Active" << endl << endl;  break;
-        case Won : std::cout << "You Won :-) \n"; break;
-        case Lost : std::cout << "You Lost :-( t\n";  break;
-    }
-
-    MakeAMove();
-    ProcessMove();
-    
-    //end of program
-    cout << endl << "Thank you for Playing" << endl << endl;
-    return 0;
-}
 
 //************************************
 //************ Functions *************
@@ -91,11 +66,28 @@ int SetupInitialBoards(int x) {
 
 int PlantMines(){
     srand( static_cast<unsigned int>(time(NULL))); //set a random seed
-    for (int m = 0; m <= MineSize-1; m++){
+    for (int m = 0; m <= NumOfMines-1; m++){
         int PlaceMineAt = rand() % (BoardSize);
         MineField[PlaceMineAt] = 'X';
+        cout << PlaceMineAt << " " ;
     }
-    
+    cout << endl;
+    int PrivateKey = 0;
+    for (int k = 0; k <= BoardDim-1; k++){
+            for (int l = 0; l <= BoardDim-1; l++){
+                /* used for debug
+                cout << PrivateKey << ": " << k << "," << l << endl;
+                */
+                if (MineField[PrivateKey] == 'X'){
+                    PrivateBoard[k][l] = 'X';
+                    /* used for debug
+                    cout << "Mine Position: " << PrivateKey << endl;
+                    cout << "Mine Placed at: " << k << "." << l << endl;
+                    */
+                 }
+                ++PrivateKey;
+            }
+    }
     return 0;
 }
 
@@ -105,24 +97,12 @@ int MakeAMove(){
     cin >> xMove;
     cout << "Make A Move (y - then enter)";
     cin >> yMove;
-/*    cout << "Press q to quit";
-    char waitforit;
-    cin >> waitforit; */
     return 0;
 }
 
-    
-int ProcessMove(){
-    cout << "you chose x:" << xMove << "and y: " << yMove << endl;
-    PublicBoard[xMove][yMove] = '#';
-    OutputBoard();
-    
-    return 0;
-    
-}
 
-int OutputBoard() {
-    cout << endl << "OutputPublicBoard:" << endl << endl ;
+int OutputPublicBoard() {
+    cout << endl << "Public Board:" << endl << endl ;
     cout << "  0 1 2 3 4 5 6 7" << endl;
     for (int k = 0; k <= BoardDim-1; k++){
         cout << k << " ";
@@ -130,9 +110,84 @@ int OutputBoard() {
             cout << PublicBoard[k][l]; // output cell contents
             cout << " ";
         }
+        cout << endl;
+        
+    }
+    cout << endl;
+    return 0;
+}
+
+// used for debug and test
+int OutputPrivateBoard() {
+    cout << endl << "Private Board:" << endl << endl ;
+    cout << "  0 1 2 3 4 5 6 7" << endl;
+    for (int k = 0; k <= BoardDim-1; k++){
+        cout << k << " ";
+        for (int l = 0; l <= BoardDim-1; l++){
+            cout << PrivateBoard[k][l]; // output cell contents
+            cout << " ";
+        }
         cout << endl ;
         
     }
+    return 0;
+}
+
+int PressEnter() {
+    cout << "Press q then Enter to quit";
+    char waitforit;
+    cin >> waitforit;
+    return 0;
+}
+
+void OutputStatus(bool here) {
+    if (here == true) {
+        cout << "STILL PLAYING" << endl;
+    }
+    else {
+        cout << "YOU LOST" << endl;
+    }
+}
+
+int ProcessMove(){
+    cout << "you chose x:" << xMove << "and y: " << yMove << endl;
+    if (PrivateBoard[xMove][yMove] == 'X'){
+        cout << "Bomb Found at " << xMove << "," << yMove << "!" << endl;
+        cout << "YOU LOSE!" << endl;
+        StillPlaying = false;
+    }
+    else {
+        PublicBoard[xMove][yMove] = '#';
+    }
+    return 0;
+    
+}
+
+//************************************
+//************ Main ******************
+//************************************
+
+int main() {
+    cout << "BoardDim: " << BoardDim << " x " << BoardDim << endl << "# of Mines: " << NumOfMines << endl;
+    SetupInitialBoards(BoardDim);
+    PlantMines();
+    OutputPrivateBoard();
+    
+    StillPlaying = true;
+    OutputStatus(StillPlaying);
+    
+    while (StillPlaying) {
+        OutputPublicBoard();
+        MakeAMove();
+        ProcessMove();
+        //OutputStatus(StillPlaying);
+    }
+    PressEnter();
+    
+
+    
+    //end of program
+    cout << endl << "Thank you for Playing" << endl << endl;
     return 0;
 }
 
